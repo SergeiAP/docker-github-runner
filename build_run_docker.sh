fe9ad52ffb54fcbc52bedb25416e1c27e1f342b2
Sergei Parshin <Sergei.A.P@yandex.com>
2022-06-20 01:48:13 +0300

    DEL network in the file

--- build_run_docker.sh
+++ build_run_docker.sh
@@ -7,7 +7,3 @@ docker run \
 	--name $( grep CONTAINER_NAME= docker.env | cut -d '=' -f2) \
 	-p 9980:80 -p 9443:443 \
 	$( grep IMAGE_NAME= docker.env | cut -d '=' -f2):latest	
-
-docker network connect \
-	$( grep DOCKER_NETWORK= docker.env | cut -d '=' -f2) \
-	$( grep CONTAINER_NAME= docker.env | cut -d '=' -f2):latest
